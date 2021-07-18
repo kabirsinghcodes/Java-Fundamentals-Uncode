ec7208b1450ef1837a704c8c94851e8d6fea8e53
kabirsingh04 <kabirsingh0405@gmail.com>
2021-07-18 18:27:44 +0530

    star10 added

---
+++ Patterns/star10.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class star10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
