b42565009f151970253fe5d7f0d035edf4039e21
kabirsingh04 <kabirsingh0405@gmail.com>
2021-07-23 17:18:56 +0530

    arrays added

---
+++ Arrays/declaration.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+public class declaration {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        System.out.println(arr[2]);
+    }
+}
