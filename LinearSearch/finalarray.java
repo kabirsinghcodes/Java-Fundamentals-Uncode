32fe1c7d783a1126fc1904652846ea0f19886730
kabirsingh04 <kabirsingh0405@gmail.com>
2021-07-24 18:23:27 +0530

    linear search added

---
+++ LinearSearch/finalarray.java
@@ -0,0 +1,33 @@
+package LinearSearch;
+
+import java.util.Scanner;
+
+public class finalarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int temp = 0;
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the " + n + " elements in the array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the element you want to search : ");
+        int search = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == search) {
+                System.out.println(search + " was found at the position : " + i);
+                temp = temp + 1;
+                break;
+            }
+        }
+        if (temp == 0) {
+            System.out.println("Element not found!");
+        }
+    }
+}
