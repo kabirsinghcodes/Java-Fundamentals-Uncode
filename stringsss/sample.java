63eb62c9ec3ea4e31aa932ffb315c398c99d4e80
kabirsingh04 <kabirsingh0405@gmail.com>
2021-07-26 18:04:49 +0530

    string added

---
+++ stringsss/sample.java
@@ -0,0 +1,9 @@
+package stringsss;
+
+public class sample {
+    public static void main(String[] args) {
+        char[] c = { 'K', 'A', 'B', 'I', 'R' };
+        String s = new String(c);
+        System.out.println(s);
+    }
+}
